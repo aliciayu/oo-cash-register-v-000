c3909f1677b49d9a96d8c62bf86d6380a1047a5a
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-13 01:12:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@
 class CashRegister
   attr_accessor :total, :discount, :title, :quantity
 
-  def initialize(discount=0)
+  def initialize(discount=200)
     @total = 0
     @discount = discount
   end
