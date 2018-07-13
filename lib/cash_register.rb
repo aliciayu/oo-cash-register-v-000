dfa0831e6cd39566d97c1724d1ccc7ba0cb3d44b
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-13 03:39:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    @items << (title * quantity.split(", "))
+    @items << (title * quantity
     @total += (price * quantity)
   end
 
@@ -22,7 +22,7 @@ class CashRegister
   end
 
   def items
-    @items
+    @items.split(", ")
   end
 
 end
