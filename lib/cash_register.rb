99989b684bfe1458ee3a1807493be43d05bf0c46
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-13 01:39:58 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount
+    if @discount > 0
       @total = (total - 200)
       "After the discount, the total comes to $800."
     elsif @discount < 0
